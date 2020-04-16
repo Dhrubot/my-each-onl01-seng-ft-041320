bc850c8aa3b372f98a671851ca3d29f2b2d5c824
Dhrubo Talukder <dhrubo.dh@gmail.com>
2020-04-16 01:14:41 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,17 @@
 def my_each(array) # put argument(s) here
   # code here
+  
   if block_given?
+    i = 0 
     
-    
-    
+    while i < array.length 
+      yield (array[i])
+      i += 1
+      
+    end
   else
     puts "No block has been given."
   end
+  
   array
 end
